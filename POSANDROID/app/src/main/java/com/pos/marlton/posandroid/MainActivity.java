4004bb57ff1a03003405658dc5e8e3243bd22594
Manuel <manuelabud07@gmail.com>
2017-08-28 13:30:02 -0400

    Dummy commit

--- POSANDROID/app/src/main/java/com/pos/marlton/posandroid/MainActivity.java
+++ POSANDROID/app/src/main/java/com/pos/marlton/posandroid/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
+        // Dummy Comment
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
